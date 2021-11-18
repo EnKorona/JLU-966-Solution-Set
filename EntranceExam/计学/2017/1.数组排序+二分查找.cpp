2d6a931b630c197111a169a6ac38667d015fbcac
EnKorona <enkorona@gmail.com>
2021-11-18 16:23:48 +0800

    2017-1

--- EntranceExam/计学/2017/1.数组排序+二分查找.cpp
+++ EntranceExam/计学/2017/1.数组排序+二分查找.cpp
@@ -1,7 +1,11 @@
+/*
+ * 1. 通过堆排序算法，将A数组调整有序
+ * 2. 通过二分查找算法循环查找A中有多少个值小于等于B[i]
+ * 3. 将该值赋值给数组C[i]
+ */
 
-void heapSort(int arr[]) {
+void heapSort(int arr[], int len) {
     // 构建大顶堆
-    int len = sizeof(arr);
     for (int i = len / 2 - 1; i >= 0; i--) { // 从第一个非叶节点开始，从下至上，从右至左调整结构
         adjustDown(arr, i, len);
     }
@@ -11,7 +15,6 @@ void heapSort(int arr[]) {
         adjustDown(arr, 0, j); // 重新对堆进行调整
     }
 }
-
 
 void adjustDown(int arr[], int i, int len) {
     int temp = arr[i];
@@ -25,9 +28,25 @@ void adjustDown(int arr[], int i, int len) {
     arr[i] = temp;
 }
 
-
 void swap(int arr[], int a, int b) {
     int temp = arr[a];
     arr[a] = arr[b];
     arr[b] = temp;
+}
+
+int binarySearch(int arr[], int len, int key) {
+    int low = 0, high = len - 1, mid;
+    while (low <= high) {
+        mid = (low + high) / 2;
+        if (arr[mid] > key) high = mid - 1;
+        else low = mid + 1;
+    }
+    return high;
+}
+
+void getArrayC(int A[], int B[], int C[], int len) {
+    heapSort(A, len);
+    for (int i = 0; i < len; i++) {
+        C[i] = binarySearch(A, n, B[i]);
+    }
 }
