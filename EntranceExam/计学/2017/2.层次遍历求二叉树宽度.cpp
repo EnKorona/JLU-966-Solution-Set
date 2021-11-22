8aeb041e8d4a71bd4599cfcd5ce8ec973fef7a32
EnKorona <enkorona@gmail.com>
2021-11-22 10:11:43 +0800

    2017-2

---
+++ EntranceExam/计学/2017/2.层次遍历求二叉树宽度.cpp
@@ -0,0 +1,28 @@
+/*
+ * 1. 采用层次遍历，初始化队列，将根节点入队，初始化最大宽度为-1
+ * 2. 当队列非空时，求出队列长度，即为当前层的节点个数，然后更新二叉树宽度
+ * 3. 对队列中的元素执行出队操作，若其左右子树不为空则依次入队
+ * 4. 反复执行2-3，直至队列清空
+ */
+
+int width(TreeNode *root) {
+    if (root == NULL) return 0;
+    int maxWidth = -1;
+    queue < TreeNode * > q;
+    q.push(root);
+    while (!q.empty()) {
+        int curWidth = q.size();    //求出当前层的元素个数
+        maxWidth = maxWidth > curWidth ? maxWidth : curWidth;   //更新二叉树宽度
+        for (int i = 0; i < curWidth; i++) {
+            TreeNode *cur = q.front();
+            q.pop();
+            if (cur->left != NULL) {
+                q.push(cur->left);
+            }
+            if (cur->right != NULL) {
+                q.push(cur->right);
+            }
+        }
+    }
+    return maxWidth;
+}
