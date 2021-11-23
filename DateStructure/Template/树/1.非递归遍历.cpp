1b4c781a01813089e14e69c6a7e041f8c31c786d
EnKorona <enkorona@gmail.com>
2021-11-23 15:47:53 +0800

    add template

---
+++ DateStructure/Template/树/1.非递归遍历.cpp
@@ -0,0 +1,64 @@
+/*
+ * 先序：先出栈，右入栈，左入栈
+ * 中序：左入栈顶头，头出指右子，再来一轮回
+ * 后序：先出栈，左入栈，右入栈，后转置
+ * 层序：先入队，左入队，右入队
+ */
+
+
+vector<int> preorderTraversal(TreeNode *root) {
+    vector<int> res;
+    if (root == NULL) return res;
+    stack < TreeNode * > stk;
+    stk.push(root);
+    while (!stk.empty()) {
+        TreeNode *node = stk.top();
+        res.push_back(node->val);
+        stk.pop();
+        if (node->right != NULL) {
+            stk.push(node->right);
+        }
+        if (node->left != NULL) {
+            stk.push(node->left);
+        }
+    }
+    return res;
+}
+
+vector<int> inorderTraversal(TreeNode *root) {
+    vector<int> res;
+    if (root == NULL) return res;
+    stack < TreeNode * > stk;
+    TreeNode *cur = root;
+    while (!stk.empty() || cur != NULL) {
+        while (cur != NULL) {
+            stk.push(cur);
+            cur = cur->left;
+        }
+        TreeNode *node = stk.top();
+        res.push_back(node->val);
+        stk.pop();
+        if (node->right != NULL) cur = node->right;
+    }
+    return res;
+}
+
+vector<int> postorderTraversal(TreeNode *root) {
+    vector<int> res;
+    if (root == NULL) return res;
+    stack < TreeNode * > stk;
+    stk.push(root);
+    while (!stk.empty()) {
+        TreeNode *cur = stk.top();
+        res.push_back(cur->val);
+        stk.pop();
+        if (cur->left != NULL) {
+            stk.push(cur->left);
+        }
+        if (cur->right != NULL) {
+            stk.push(cur->right);
+        }
+    }
+    reverse(res.begin(), res.end());
+    return res;
+}
