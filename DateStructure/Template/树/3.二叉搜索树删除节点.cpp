bba2100004a5a2a1fb7c9a7521a67732a3500ffe
EnKorona <enkorona@gmail.com>
2021-11-24 16:31:52 +0800

    add template

---
+++ DateStructure/Template/树/3.二叉搜索树删除节点.cpp
@@ -0,0 +1,27 @@
+/*
+ *
+ */
+
+TreeNode *deleteNode(TreeNode *root, int key) {
+    if (root == nullptr) return nullptr;
+    if (root->val == key) {
+        if (root->left == nullptr) return root->right;
+        if (root->right == nullptr) return root->left;
+        TreeNode *rightMin = getMin(root->right);
+        root->val = rightMin->val;
+        root->right = deleteNode(root->right, rightMin->val);
+    }
+    if (root->val > key) {
+        root->left = deleteNode(root->left, key);
+    }
+    if (root->val < key) {
+        root->right = deleteNode(root->right, key);
+    }
+}
+
+TreeNode *getMin(TreeNode *node) {
+    while (node->left != nullptr) {
+        node = node->left;
+    }
+    return node;
+}
