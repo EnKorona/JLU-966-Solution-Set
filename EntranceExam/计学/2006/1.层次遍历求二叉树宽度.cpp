42943859b4b515b56e784538b3196a0685093528
EnKorona <enkorona@gmail.com>
2021-11-28 23:54:35 +0800

    fix

--- EntranceExam/计学/2006/1.层次遍历求二叉树宽度.cpp
+++ EntranceExam/计学/2006/1.层次遍历求二叉树宽度.cpp
@@ -2,7 +2,7 @@
  * 1. 采用层次便利，初始化最大宽度为0，初始化队列，将根节点入队
  * 2. 用curLevelSize表示当前层的节点个数，当curLevelSize的值大于max时，将max更新
  * 3. 反复执行出队，并将左右节点入队，重复2，直到队列中元素全部出队
- * 【leetcode】类似0102
+ * 【leetcode】类似0102,同2017(2)
  */
 
 int levelOrder(TreeNode *root) {
@@ -11,8 +11,8 @@ int levelOrder(TreeNode *root) {
     queue < TreeNode * > q;
     q.push(root);
     while (!q.empty()) {
-        int curLevelSize = q.size();
-        max = curLevelSize > max ? curLevelSize : max;
+        int curLevelSize = q.size(); //求出当前层的元素个数
+        max = curLevelSize > max ? curLevelSize : max; //更新二叉树宽度
         for (int i = 0; i < curLevelSize; i++) {
             TreeNode *cur = q.front();
             q.pop();
--- EntranceExam/计学/2017/2.层次遍历求二叉树宽度.cpp
+++ EntranceExam/计学/2017/2.层次遍历求二叉树宽度.cpp
@@ -1,28 +1,24 @@
 /*
- * 1. 采用层次遍历，初始化队列，将根节点入队，初始化最大宽度为-1
- * 2. 当队列非空时，求出队列长度，即为当前层的节点个数，然后更新二叉树宽度
- * 3. 对队列中的元素执行出队操作，若其左右子树不为空则依次入队
- * 4. 反复执行2-3，直至队列清空
+ * 1. 采用层次便利，初始化最大宽度为0，初始化队列，将根节点入队
+ * 2. 用curLevelSize表示当前层的节点个数，当curLevelSize的值大于max时，将max更新
+ * 3. 反复执行出队，并将左右节点入队，重复2，直到队列中元素全部出队
+ * 【leetcode】类似0102,同2017(2)
  */
 
-int width(TreeNode *root) {
+int levelOrder(TreeNode *root) {
+    int max = 0;
     if (root == nullptr) return 0;
-    int maxWidth = -1;
     queue < TreeNode * > q;
     q.push(root);
     while (!q.empty()) {
-        int curWidth = q.size();    //求出当前层的元素个数
-        maxWidth = maxWidth > curWidth ? maxWidth : curWidth;   //更新二叉树宽度
-        for (int i = 0; i < curWidth; i++) {
+        int curLevelSize = q.size(); //求出当前层的元素个数
+        max = curLevelSize > max ? curLevelSize : max; //更新二叉树宽度
+        for (int i = 0; i < curLevelSize; i++) {
             TreeNode *cur = q.front();
             q.pop();
-            if (cur->left != nullptr) {
-                q.push(cur->left);
-            }
-            if (cur->right != nullptr) {
-                q.push(cur->right);
-            }
+            if (cur->left != nullptr) q.push(cur->left);
+            if (cur->right != nullptr) q.push(cur->right);
         }
     }
-    return maxWidth;
+    return max;
 }
