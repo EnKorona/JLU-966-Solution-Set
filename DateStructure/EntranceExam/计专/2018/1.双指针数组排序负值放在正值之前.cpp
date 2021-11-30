55de3f17979b62696662a415081f7379e979cae6
EnKorona <enkorona@gmail.com>
2021-11-30 17:28:37 +0800

    2017-3

---
+++ DateStructure/EntranceExam/计专/2018/1.双指针数组排序负值放在正值之前.cpp
@@ -0,0 +1,20 @@
+/*
+ * 1. 使用双指针，指针i、j分别指向数组第一个值和最后一个值，向中间移动
+ * 2. 当i和j未相遇时，i查找相遇点大于0的数，j查找相遇点右侧小于0的数，交换i和j所指的函数值
+ * 3. 重复步骤2，直至i和j相遇，则相遇点左侧全部为偶数，右侧全部为奇数
+ * 【leetcode】0905
+ * 【计学】2009-1
+ */
+
+void sortArrayByParity(int a[], int n) {
+    int i = 0, j = n - 1;
+    while (i < j) {
+        while (i < j && a[i] < 0) i++;
+        while (i < j && a[j] > 0)j--;
+        if (i < j) {
+            int temp = a[i];
+            a[i] = a[j];
+            a[j] = temp;
+        }
+    }
+}
