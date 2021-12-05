7c02df375e6e7a7bed646533b5a9ab9948516453
EnKorona <enkorona@gmail.com>
2021-12-05 09:49:28 +0800

    2014-4

---
+++ ProgrammingLanguage/EntranceExam/计专/2014/4.二维数组对角线赋值.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a[10][10];
+    int num = 1;
+    int row, column;
+    for (int i = 0; i < 10; i++) {
+        row = i, column = 0;
+        while (column >= 0) {
+            a[row--][column++] = num++;
+        }
+    }
+    for (int i = 1; i < 10; i++) {
+        row = 9, column = i;
+        while (column <= 9) {
+            a[row--][column++] = num++;
+        }
+    }
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            printf("%3d", a[i][j]);
+        }
+        printf("\n");
+    }
+}
