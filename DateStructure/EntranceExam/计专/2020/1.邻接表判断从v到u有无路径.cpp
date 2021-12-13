c4d721ebdca5c6245bf7e4d6e418dc01d21db351
EnKorona <enkorona@gmail.com>
2021-12-13 21:24:43 +0800

    nothing

--- DateStructure/EntranceExam/计专/2020/1.邻接表判断从v到u有无路径.cpp
+++ DateStructure/EntranceExam/计专/2020/1.邻接表判断从v到u有无路径.cpp
@@ -1,26 +1,22 @@
-
-struct ArcNode { // 边
-    int adjVex; // 邻接点在数组中的位置下标
-    ArcNode *nextArc; // 指向下一个邻接点的指针
-    int weight; // 权值
+struct ArcNode {
+    int adjVex;
+    int weight;
+    ArcNode *nextArc;
 }
-
-struct VNode { // 顶点
-    int data; // 顶点的数据域
-    ArcNode *firstArc; // 指向邻接点的指针
+struct VNode {
+    int data;
+    ArcNode *firstArc;
 }
-
 struct Graph {
-    AdjList vexList[]; // 图中顶点的数组
-    int vexnum,arcnum; // 图中的顶点数和弧数
+    VNode vexList[];
+    int vexnum, arcnum;
 }
 
-// v为起点，u为终点
-bool find(int v,int u) {
-    ArcNode *p = g -> vecList[v].firstArc; // p指向第一个邻接点
-    while(p != nullptr) {
-        if(p -> adjVex == u) return true;
-        p = p -> nextArc;
+bool find(int v, int u) {
+    ArcNode *p = g->vexList[v].firstArc;
+    while (p != nullptr) {
+        if (p->adjVex == u) return true;
+        p = p->nextArc;
     }
     return false;
 }
