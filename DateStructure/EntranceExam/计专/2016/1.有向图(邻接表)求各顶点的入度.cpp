816c039eb253c185277e401324e372800ea58113
EnKorona <enkorona@gmail.com>
2021-12-13 21:37:12 +0800

    refactor

--- DateStructure/EntranceExam/计专/2016/1.有向图(邻接表)求各顶点的入度.cpp
+++ DateStructure/EntranceExam/计专/2016/1.有向图(邻接表)求各顶点的入度.cpp
@@ -1,27 +1,29 @@
-struct ArcNode { // 边
-    int adjVex; // 邻接点在数组中的位置下标
-    ArcNode *nextArc; // 指向下一个邻接点的指针
+struct ArcNode { // 边表节点
+    int adjVex; // 邻接点域，存储该顶点对应的下标
     int weight; // 权值
+    ArcNode *nextArc; // 链域，指向下一个邻接点的指针
 }
 
-struct VNode { // 顶点
-    int data; // 顶点的数据域
-    ArcNode *firstArc; // 指向邻接点的指针
+struct VNode { // 顶点表节点
+    int data; // 顶点域，存储顶点信息
+    ArcNode *firstArc; // 边表头指针
 }
 
 struct Graph {
-    AdjList vexList[]; // 图中顶点的数组
+    VNode vexList[]; // 图中顶点的数组
     int vexnum, arcnum; // 图中的顶点数和弧数
 }
 
+
 int *count(Graph g) {
-    int *res = new int[g.vexnum + 1];
-    memset(res, 0, sizeof(int) * (g.vexnum + 1));
+    int *res = new int[g.vexnum];
+    memset(indegree, 0, sizeof(int) * (g.vexnum + 1));
     for (int i = 1; i <= g.vexnum; i++) {
-        ArcNode *p = g.adjlist[i].firstArc;
+        ArcNode *p = g.vexList[i].firstArc;
         while (p != nullptr) {
-            indegree[p->adjVex]++;
+            res[p->adjVex]++;
             p = p->nextArc;
         }
-        return res;
     }
+    return res;
+}
